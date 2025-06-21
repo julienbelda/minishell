2a9dc00f5a3d4c729cc81e734e4903a789ef3912
Charlie <sarfaticharlie@gmail.com>
2025-06-22 01:29:18 +0200

    ft_echo_fix1

--- srcs/execution/builtin/ft_echo.c
+++ srcs/execution/builtin/ft_echo.c
@@ -2,11 +2,46 @@
 
 // echo -> je dois afficher les arguments separé par un espace
 
+int parse_option(char *str)
+{
+	// checker si le deuxieme arg est l'option -n
+	// DANS la fonction echo, si parse option est vrai -> je dois pas afficher de saut de ligne
+	// il peut y avoir plusieurs n, exemple : -nnnnn fonctionne
+
+	int i;
+	
+	i = 0;
+	if (str[i] == '-')
+	{
+		i++;
+		while (str[i] && str[i] == 'n')
+		{
+			i++;
+			if (str[i] == '\0')
+				return 1;
+		}
+		if (str[i] != 'n')
+			return 0;
+	}
+	else
+		return 0;
+	return 0;
+}
+
 void builtin_echo(char **argv)
 {
     int i;
+	int newline;
 
 	i = 1;
+	newline = 1;
+	if (parse_option(argv[1]))
+	{
+		i++;
+		while (argv[i] && parse_option(argv[i]))
+			i++;
+		newline = 0;
+	}
 	while (argv[i])
 	{
 		printf("%s", argv[i]);
@@ -14,6 +49,8 @@ void builtin_echo(char **argv)
 			printf(" ");
 		i++;
 	}
+	if (!newline)
+		return;
 	printf("\n");
 }
 
@@ -28,7 +65,7 @@ int main(int argc, char **argv)
 	else
 	{
 		if (ft_strcmp(argv[1], "echo") == 0)
-			builtin_echo(argv + 1);
+				builtin_echo(argv + 1);
 		else
 		{
 			printf("commande inconnu");
